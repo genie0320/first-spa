13366e7a6f7b4ae32d8e9ecd46f26eba955758e2
Genie <genie0320@gmail.com>
2021-10-27 10:57:34 +0900

    First try - map()

--- src/App.js
+++ src/App.js
@@ -6,20 +6,27 @@ import { useState } from 'react'
 function App() {
 
   const [name, setName] = useState('Genie')
-  const const [events, setEvents] = useState('initialState')
+  const const [events, setEvents] = useState([
+    ('First event', id:1)
+    ('Secend event', id:2)
+    ('Third event', id:3)
+  ])
   const handleClick = () => {
     setName('Nyao!')
     console.log(name)
-
-    // 질문 : go to 'React rendering.md'
-    // 이렇게 useState() 를 사용해도, 저 위에 콘솔로그의 값은 여전히 Genie에 머물러 있다.
-    // 분명setName()으로 값을 바꿔줬는데, console.log에는 이전값이 들어가는 것이다. 왜???
-  }
 
   return (
     <div className="App">
       <h1>My name is {name}</h1>
       <button onClick={ handleClick }>Change name</button>
+
+      <div>
+        <h2>
+          map(event) in events = {() => (
+            event, key={event.id}
+        )}</h2>
+      </div>
+      
     </div>
   );
 }
