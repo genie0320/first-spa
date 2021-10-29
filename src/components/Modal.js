244e921125bcd402a6fce29339bc0794b5ba5477
Genie <genie0320@gmail.com>
2021-10-29 17:18:37 +0900

    conditional styling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,14 +1,16 @@
 import ReactDOM from 'react-dom'
 import './Modal.css'
 
-export default function Modal({ children, handleClose }) {
+export default function Modal({ children, handleClose, isSalesModal }) {
     return ReactDOM.createPortal((
         <div className="modal-dim">
 
             {/* inline styling 1 */}
             <div className="modal" style={{
                 border: "4px solid",
-                borderColor: "lightGreen",
+                // borderColor: "lightGreen",
+                // 이렇게 전통적인 수식을 통해서 간단히 조건적으로 스타일링도 가능함.
+                borderColor: isSalesModal ? "lightGreen" : "gray",
                 textAlign: "center"
             }}>
                 {children}
