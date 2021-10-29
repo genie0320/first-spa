f39678053636f1a5780940c8ae7e9b782aaf8951
Genie <genie0320@gmail.com>
2021-10-29 18:42:02 +0900

    onChange() function.

--- src/components/AddNew.js
+++ src/components/AddNew.js
@@ -1,24 +1,36 @@
+import { useState } from 'react'
 import style from './AddNew.css'
 
 export default function AddNew() {
+
+    const [title, setTitle] = useState('')
+    const [date, setDate] = useState('')
+
+    // 아래와 같이 진행할 수도 있지만... 이렇게 되면 결국 title, date의 change에 대한 처리를 하기 위해 더 복잡한 과정을 거쳐야 하므로... inline으로 넣기로 한다.
+    // const handleChange = (e) => {
+    //     console.log(e.target.value)
+    // }
+
     return (
-        <div>
+        < div >
             {/* 방법1
             <form className="add-new-event">
                 <label htmlFor="">Event title</label>
                 <input type="text" id="title" />
             </form>  */}
 
-            <form className="add-new-event">
+            < form className="add-new-event" >
                 <label>
                     <span>Event Name</span>
-                    <input type="text" />
+                    {/* <input type="text" onChange={handleChange} /> */}
+                    <input type="text" onChange={(e) => setTitle(e.target.value)} />
                 </label>
                 <label>
                     <span>Event Date</span>
-                    <input type="Date" />
+                    <input type="Date" onChange={(e) => setDate(e.target.value)} />
                 </label>
-            </form>
-        </div>
+                <p>Title - {title}, Date - {date} </p>
+            </form >
+        </div >
     )
 }
