994de9f04e450d74edbfe659b91fc513314fd389
Genie <genie0320@gmail.com>
2021-10-30 23:55:07 +0900

    useRef v. useState

--- src/components/AddNew.js
+++ src/components/AddNew.js
@@ -1,22 +1,29 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import style from './AddNew.css'
+
+// useRef()도 React의 Hook의 하나이다. 
+// - useState는 component의 state를 관리하기 위한 것.
+// - useRef는 객체를 반환하고 그 내부의 값들에 전통적인 js의 방식으로 접근할 수 있다.
+// - 듣기로, 렌더링타이밍과 관련하여 많은 차이가 있다고 한다.
 
 export default function AddNew({ addEvent }) {
 
-    const [title, setTitle] = useState('')
-    const [date, setDate] = useState('')
+    const title = useRef()
+    const date = useRef()
 
     const resetValue = () => {
-        setTitle('')
-        setDate('')
+        title.current.value = ""
+        date.current.value = ""
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
 
+        // console.log(title, date)
+
         const event = {
-            title: title,
-            date: date,
+            title: title.current.value,
+            date: date.current.value,
             id: Math.floor(Math.random() * 10000)
         }
 
@@ -30,16 +37,14 @@ export default function AddNew({ addEvent }) {
                 <span>Event Name</span>
                 <input
                     type="text"
-                    onChange={(e) => setTitle(e.target.value)}
-                    value={title}
+                    ref={title}
                 />
             </label>
             <label>
                 <span>Event Date</span>
                 <input
                     type="date"
-                    onChange={(e) => setDate(e.target.value)}
-                    value={date}
+                    ref={date}
                 />
             </label>
             <button>Submit</button>
